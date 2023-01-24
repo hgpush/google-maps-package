fe30abe8338929bf830d895b026c793f0fa61770
hgpush <gitpush@google.com>
2023-01-24 13:22:09 -0800

    add dependencies

--- Package.swift
+++ Package.swift
@@ -11,12 +11,17 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(name: "GoogleMaps", targets: ["GoogleMaps"]),
     ],
+    dependencies: [
+    	.package(name: "GoogleMapsBase", url: "https://github.com/hgpush/google-maps-base.git")
+    	.package(name: "GoogleMapsCore", url: "https://github.com/hgpush/google-maps-core.git")
+    ]
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
             name: "GoogleMaps",
+            dependencies: ["GoogleMapsBase", "GoogleMapsCore"],
             path: "Sources/GoogleMapsSPM/GoogleMaps.xcframework"
-				),
+		),
     ]
 )

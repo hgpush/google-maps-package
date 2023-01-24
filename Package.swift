7a56456eed751baf2287fa1ad3e571a1c39385e5
hgpush <gitpush@google.com>
2023-01-24 13:37:00 -0800

    wrapper target for deps

--- Package.swift
+++ Package.swift
@@ -9,19 +9,22 @@ let package = Package(
 		platforms: [.iOS(.v13)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(name: "GoogleMaps", targets: ["GoogleMaps"]),
+        .library(name: "GoogleMapsPackage", targets: ["GoogleMaps", "GoogleMapsDeps"]),
     ],
     dependencies: [
     	.package(name: "GoogleMapsBase", url: "https://github.com/hgpush/google-maps-base.git", .branch("main"))
-    	.package(name: "GoogleMapsCore", url: "https://github.com/hgpush/google-maps-core.git", .branch("main"))
+    	.package(name: "GoogleMapsCore", url: "https://github.com/hgpush/google-maps-core.git",	 .branch("main"))
     ]
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
             name: "GoogleMaps",
-            dependencies: ["GoogleMapsBase", "GoogleMapsCore"],
             path: "Sources/GoogleMapsSPM/GoogleMaps.xcframework"
-		),	
+		),
+		.target(
+			name: "GoogleMapsDeps",
+			dependencies: ["GoogleMapsBase", "GoogleMapsCore"],
+		)
     ]
 )

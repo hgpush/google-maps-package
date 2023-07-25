1109e50671cb3a368332c0e50d45c912846992cf
hgpush <103077424+hgpush@users.noreply.github.com>
2023-07-25 18:16:05 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,35 +4,36 @@
 import PackageDescription
 
 let package = Package(
-    name: "GoogleMaps",
-		defaultLocalization: "en",
-		platforms: [.iOS(.v13)],
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-			.library(name: "GoogleMapsPackage", targets: ["GoogleMaps", "GoogleMapsDeps"]),
-    ],
-    dependencies: [
-			.package(url: "https://github.com/hgpush/google-maps-base.git", branch: "main"),
-			.package(url: "https://github.com/hgpush/google-maps-core.git",	branch: "main")
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-			.binaryTarget(
-					name: "GoogleMaps",
-					url: "https://github.com/hgpush/google-maps-package/releases/download/1.0.0/GoogleMaps.xcframework.zip",
-					checksum: "d1b06fcd340cf9b10be3f0a659c6c83de28f8e2ceb8fa53df42cbcc22f345fde"
-			),
-			.target(
-				name: "GoogleMapsDeps",
-				dependencies: [
-          .product(name: "GoogleMapsBase", package: "google-maps-base"),
-          .product(name: "GoogleMapsCore", package: "google-maps-core")
-        ],
-        sources: ["empty.swift"],
-				resources: [
-					.copy("Resources/GoogleMaps.bundle")
-				]
-			)
-    ]
+	name: "GoogleMaps",
+	defaultLocalization: "en",
+	platforms: [.iOS(.v13)],
+    	products: [
+        	// Products define the executables and libraries a package produces, and make them visible to other packages.
+		.library(name: "GoogleMaps", targets: ["GoogleMapsTarget"]),
+    	],
+	dependencies: [
+		.package(url: "https://github.com/hgpush/google-maps-base.git", branch: "main"),
+		.package(url: "https://github.com/hgpush/google-maps-core.git",	branch: "main")
+	],
+	targets: [
+	// Targets are the basic building blocks of a package. A target can define a module or a test suite.
+	// Targets can depend on other targets in this package, and on products in packages this package depends on.
+		.binaryTarget(
+			name: "GoogleMaps",
+			url: "https://github.com/hgpush/google-maps-package/releases/download/1.0.0/GoogleMaps.xcframework.zip",
+			checksum: "d1b06fcd340cf9b10be3f0a659c6c83de28f8e2ceb8fa53df42cbcc22f345fde"
+		),
+		.target(
+			name: "GoogleMapsTarget",
+			dependencies: [
+		  		.product(name: "GoogleMapsBase", package: "google-maps-base"),
+		  		.product(name: "GoogleMapsCore", package: "google-maps-core"),
+				"GoogleMaps"
+			],
+			sources: ["empty.swift"],
+			resources: [
+				.copy("Resources/GoogleMaps.bundle")
+			]
+		)
+	]
 )

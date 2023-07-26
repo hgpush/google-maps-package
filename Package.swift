630ce459bdce5cce1d25596f7a16495e22caef70
Pushkar Sharma <gitpush@google.com>
2023-07-26 14:35:48 -0400

    rewrite package.swift

--- Package.swift
+++ Package.swift
@@ -4,6 +4,45 @@
 import PackageDescription
 
 let package = Package(
+  name: "GoogleMaps",
+  products: [
+    // Products define the executables and libraries a package produces, and make them visible to other packages.
+    .library(
+      name: "GoogleMaps",
+      targets: ["GoogleMapsTarget"]),
+  ],
+  dependencies: [
+    // Dependencies declare other packages that this package depends on.
+    // .package(url: /* package url */, from: "1.0.0"),
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+    // Targets can depend on other targets in this package, and on products in packages this package depends on.
+    .binaryTarget(
+      name: "GoogleMaps",
+      url: "https://github.com/hgpush/google-maps-package/releases/download/1.0.0/GoogleMaps.xcframework.zip",
+      checksum: "d1b06fcd340cf9b10be3f0a659c6c83de28f8e2ceb8fa53df42cbcc22f345fde"
+    ),
+    .target(
+      name: "GoogleMapsTarget",
+      dependencies: ["GoogleMaps"],
+      path: "GoogleMaps",
+      sources: ["dummy.m"],
+      resources: [
+        .copy("Resources/GoogleMaps.bundle")
+      ],
+      publicHeadersPath: "Sources"
+    ),
+  ]
+)
+
+/*
+// swift-tools-version: 5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
 	name: "GoogleMaps",
 //	defaultLocalization: "en",
 //	platforms: [.iOS(.v14)],
@@ -65,3 +104,4 @@ let package = Package(
 //    )
 	]
 )
+*/

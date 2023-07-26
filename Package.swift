f94f81ebbf8f144c77849fd12bf7afb0aae5c564
Pushkar Sharma <gitpush@google.com>
2023-07-26 13:03:54 -0400

    Update product target names

--- Package.swift
+++ Package.swift
@@ -17,26 +17,26 @@ let package = Package(
 	// Targets are the basic building blocks of a package. A target can define a module or a test suite.
 	// Targets can depend on other targets in this package, and on products in packages this package depends on.
 		.binaryTarget(
-			name: "GoogleMaps",
+			name: "GoogleMapsBinary",
 			url: "https://github.com/hgpush/google-maps-package/releases/download/1.0.0/GoogleMaps.xcframework.zip",
 			checksum: "d1b06fcd340cf9b10be3f0a659c6c83de28f8e2ceb8fa53df42cbcc22f345fde"
 		),
     .binaryTarget(
-        name: "GoogleMapsCore",
+        name: "GoogleMapsCoreBinary",
         url: "https://github.com/hgpush/google-maps-core/releases/download/1.0.0/GoogleMapsCore.xcframework.zip",
         checksum: "cfd2d8f07e08fd3882e10fb5f0910e129dbbbec8b17932ced99c30c965febe53"
     ),
     .binaryTarget(
-        name: "GoogleMapsBase",
+        name: "GoogleMapsBaseBinary",
         url: "https://github.com/hgpush/google-maps-base/releases/download/1.0.0/GoogleMapsBase.xcframework.zip",
         checksum: "5139b6693ef1529ccd5bb2fdc2c91b30992ffc60f77901d01acc969865aae8ef"
     ),
 		.target(
 			name: "GoogleMapsPackage",
 			dependencies: [
-        "GoogleMapsBase",
-				"GoogleMaps",
-        "GoogleMapsCore"
+        "GoogleMapsBaseBinary",
+				"GoogleMapsBinary",
+        "GoogleMapsCoreBinary"
 			],
       path: "GoogleMaps",
 			sources: ["dummy.m"],

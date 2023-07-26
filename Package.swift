e51b9bbbb79a1631a9b408e359fc3537da6252b2
Pushkar Sharma <gitpush@google.com>
2023-07-26 15:18:32 -0400

    update package.swift with dependency packages for core and base

--- Package.swift
+++ Package.swift
@@ -4,53 +4,14 @@
 import PackageDescription
 
 let package = Package(
-  name: "GoogleMaps",
-  products: [
-    // Products define the executables and libraries a package produces, and make them visible to other packages.
-    .library(
-      name: "GoogleMaps",
-      targets: ["GoogleMapsTarget"]),
-  ],
-  dependencies: [
-    // Dependencies declare other packages that this package depends on.
-    // .package(url: /* package url */, from: "1.0.0"),
-  ],
-  targets: [
-    // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-    // Targets can depend on other targets in this package, and on products in packages this package depends on.
-    .binaryTarget(
-      name: "GoogleMaps",
-      url: "https://github.com/hgpush/google-maps-package/releases/download/1.0.1/GoogleMaps_3p.xcframework.zip",
-      checksum: "93cc594719cdfcc993e7e6fa06fa989f033fd412f92b0c8e2b148ff7a2b6fb9c"
-    ),
-    .target(
-      name: "GoogleMapsTarget",
-      dependencies: ["GoogleMaps"],
-      path: "GoogleMaps",
-      sources: ["dummy.m"],
-      resources: [
-        .copy("Resources/GoogleMaps.bundle")
-      ],
-      publicHeadersPath: "Sources"
-    ),
-  ]
-)
-
-/*
-// swift-tools-version: 5.7
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
-import PackageDescription
-
-let package = Package(
 	name: "GoogleMaps",
 //	defaultLocalization: "en",
 //	platforms: [.iOS(.v14)],
     	products: [
         	// Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(name: "GoogleMaps", targets: ["GoogleMapsPackage"]),
-//        .library(name: "GoogleMapsBase", targets: ["GoogleMapsBasePackage"]),
-//        .library(name: "GoogleMapsCore", targets: ["GoogleMapsCorePackage"]),
+        .library(name: "GoogleMapsBase", targets: ["GoogleMapsBasePackage"]),
+        .library(name: "GoogleMapsCore", targets: ["GoogleMapsCorePackage"]),
     	],
 	dependencies: [
 	],
@@ -59,19 +20,19 @@ let package = Package(
 	// Targets can depend on other targets in this package, and on products in packages this package depends on.
 		.binaryTarget(
 			name: "GoogleMaps",
-			url: "https://github.com/hgpush/google-maps-package/releases/download/1.0.0/GoogleMaps.xcframework.zip",
-			checksum: "d1b06fcd340cf9b10be3f0a659c6c83de28f8e2ceb8fa53df42cbcc22f345fde"
+			url: "https://github.com/hgpush/google-maps-package/releases/download/1.0.1/GoogleMaps_3p.xcframework.zip",
+			checksum: "93cc594719cdfcc993e7e6fa06fa989f033fd412f92b0c8e2b148ff7a2b6fb9c"
 		),
-//    .binaryTarget(
-//        name: "GoogleMapsCore",
-//        url: "https://github.com/hgpush/google-maps-core/releases/download/1.0.0/GoogleMapsCore.xcframework.zip",
-//        checksum: "cfd2d8f07e08fd3882e10fb5f0910e129dbbbec8b17932ced99c30c965febe53"
-//    ),
-//    .binaryTarget(
-//        name: "GoogleMapsBase",
-//        url: "https://github.com/hgpush/google-maps-base/releases/download/1.0.0/GoogleMapsBase.xcframework.zip",
-//        checksum: "5139b6693ef1529ccd5bb2fdc2c91b30992ffc60f77901d01acc969865aae8ef"
-//    ),
+    .binaryTarget(
+        name: "GoogleMapsCore",
+        url: "https://github.com/hgpush/google-maps-package/releases/download/1.0.1/GoogleMapsCore_3p.xcframework.zip",
+        checksum: "eef29bd7107889482ecbd7c60fd3eedfc2d53a6720bf8deee09b1eb20f1afd81"
+    ),
+    .binaryTarget(
+        name: "GoogleMapsBase",
+        url: "https://github.com/hgpush/google-maps-package/releases/download/1.0.1/GoogleMapsBase_3p.xcframework.zip",
+        checksum: "af10b10661a5ee4d476050239573cfe2abc6e1148c14de650a88606e20aefe94"
+    ),
 		.target(
 			name: "GoogleMapsPackage",
 			dependencies: [
@@ -83,25 +44,24 @@ let package = Package(
 				.copy("Resources/GoogleMaps.bundle")
       ],
       publicHeadersPath: "Sources"
-		)
-//    .target(
-//      name: "GoogleMapsBasePackage",
-//      dependencies: [
-//        "GoogleMapsBase",
-//      ],
-//      path: "GoogleMapsBase",
-//      sources: ["dummy.m"],
-//      publicHeadersPath: "Sources"
-//    ),
-//    .target(
-//      name: "GoogleMapsCorePackage",
-//      dependencies: [
-//        "GoogleMapsCore"
-//      ],
-//      path: "GoogleMapsCore",
-//      sources: ["dummy.m"],
-//      publicHeadersPath: "Sources"
-//    )
+		),
+    .target(
+      name: "GoogleMapsBasePackage",
+      dependencies: [
+        "GoogleMapsBase",
+      ],
+      path: "GoogleMapsBase",
+      sources: ["dummy.m"],
+      publicHeadersPath: "Sources"
+    ),
+    .target(
+      name: "GoogleMapsCorePackage",
+      dependencies: [
+        "GoogleMapsCore"
+      ],
+      path: "GoogleMapsCore",
+      sources: ["dummy.m"],
+      publicHeadersPath: "Sources"
+    )
 	]
 )
-*/
